08b30092082a12b3c060cbc859b3cdaa0566ef43
Badra Mohamed <moha1150@umn.edu>
2022-05-17 16:19:22 -0500

    save my work

---
+++ server/public/client.js
@@ -0,0 +1,16 @@
+$(document).ready(onReady)
+
+
+
+console.log('look ma im a client');
+
+function onReady (){
+    console.log('document is working');
+
+    $('body').append(`<h2> Jquery time </h2>`);
+
+   // $('img'),css('height', '250');
+}
+
+
+
